af27e83ed1671fae36d11001804fdc266fff4af1
kochiling <p21013435@student.newinti.edu.my>
2025-06-30 11:33:00 +0800

    Update the post display and deleted unused file

--- lib/my_database_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
-
-class MyDatabaseService {
-  static final FirebaseDatabase db = FirebaseDatabase.instanceFor(
-    app: Firebase.app(), // Get the default initialized app
-    databaseURL: 'https://thinktwice-clzy-default-rtdb.asia-southeast1.firebasedatabase.app',
-  );
-}
